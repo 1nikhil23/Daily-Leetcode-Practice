6b2583fadd61e8fbc99154caef77f066829a1b55
Nikhil Sharma <160346385+1nikhil23@users.noreply.github.com>
2026-09-12 16:14:19 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.31%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,19 +12,11 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        //Both nodes are null, so they are identical
-        if (p == nullptr && q == nullptr) {
-            return true;
+        if(p==NULL||q==NULL){
+            return (p==q);
         }
-        //  One node is null and the other isn't, so they are different
-        if (p == nullptr || q == nullptr) {
-            return false;
-        }
-        //  The values match, so check their left and right subtrees
-        if (p->val == q->val) {
-            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-        }
-        // Values don't match
-        return false;
+        return (p->val==q->val)
+        && isSameTree(p->left,q->left) 
+        && isSameTree(p->right,q->right);
     }
 };
