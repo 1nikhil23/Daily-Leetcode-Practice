fc9a299789843106c9827a36cd2056ed384e387e
Nikhil Sharma <160346385+1nikhil23@users.noreply.github.com>
2026-09-15 14:54:31 +0530

    Time: 0 ms (100%), Space: 14.9 MB (63.9%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -17,11 +17,11 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            int size = q.size();
-            for (int i = 0; i <size; i++) {
+            int n = q.size();
+            for (int i = 0; i <n; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                if (i ==size - 1){
+                if (i ==n-1){
                     ans.push_back(node->val);
                 }
                 if(node->left!=NULL){ 
